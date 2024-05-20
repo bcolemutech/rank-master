9cb33303af52768ed63c969b8ac3df72e5a11fbc
BCole <bcolemutech@hotmail.com>
2024-05-19 22:44:09 -0500

    Port rank calculation from hst-cli (#8)

---
+++ RankMaster/Services/RankService.cs
@@ -0,0 +1,18 @@
+﻿namespace RankMaster.Services;
+
+public class RankService
+{
+    public static decimal CalculateRank(int plays, int wins)
+    {
+        if (plays > 20)
+        {
+            throw new ArgumentOutOfRangeException(nameof(plays), "Plays must be less than 20");
+        }
+            
+        var toPlay = 20 - plays;
+        var adjustedWins = wins + toPlay * 0.25m;
+        var winPercentage = adjustedWins / 20 * 100;
+        var rank = (winPercentage + 40) / 20;
+        return Math.Round(rank, 0);
+    }
+}
