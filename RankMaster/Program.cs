2e1e4d9367316b9dc4e2b13a5d836c7ca66152ef
BCole <bcolemutech@hotmail.com>
2024-05-19 22:36:45 -0500

    2 create startup spectre app (#7)
    * New solution file
    
    * New project for console app
    
    * Test project
    
    * Basic welcome screen

---
+++ RankMaster/Program.cs
@@ -0,0 +1,7 @@
+﻿using Spectre.Console;
+
+AnsiConsole.Write(new FigletText("Welcome to RankMaster!").LeftJustified().Color(Color.Green));
+
+AnsiConsole.MarkupLine("Welcome to [bold green]RankMaster[/]!");
+AnsiConsole.MarkupLine("Type any key to continue...");
+Console.ReadKey();
