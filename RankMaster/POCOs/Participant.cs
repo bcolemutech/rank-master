a4efcbb929e19c7299d0f481029093edb1c479cf
bcole <bcole@hy-vee.com>
2024-06-04 16:31:24 -0500

    Add properties to participants

--- RankMaster/POCOs/Participant.cs
+++ RankMaster/POCOs/Participant.cs
@@ -1,11 +1,35 @@
 ﻿namespace RankMaster.POCOs;
 
+using System.Text.Json.Serialization;
+
+[JsonSerializable(typeof(Participant))]
 public class Participant
-{
-    public State States { get; set; }
+{ 
+    [JsonPropertyName("id")]
+    public string Id { get; set; } = string.Empty;
+
+    [JsonPropertyName("attributes")]
+    public ParticipantAttributes Attributes { get; set; } = new();
+
+    [JsonPropertyName("wins")]
+    public int Wins { get; set; }
+
+    [JsonPropertyName("losses")]
+    public int Losses { get; set; }
+    
+    [JsonIgnore]
+    public decimal WinPercentage => Wins + Losses == 0 ? 0 : Math.Round((decimal)Wins / (Wins + Losses) * 100, 2);
+
+    [JsonPropertyName("handicap")]
+    public int Handicap { get; set; }
 }
 
-public struct State
+[JsonSerializable(typeof(ParticipantAttributes))]
+public class ParticipantAttributes
 {
-    public bool Active { get; set; }
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [JsonPropertyName("username")]
+    public string Username { get; set; }
 }
