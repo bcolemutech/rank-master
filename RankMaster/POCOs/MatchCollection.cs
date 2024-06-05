c7edd0cb85f3addb42ec43d0d15c64b130299cf6
bcole <bcolemutech@gmail.com>
2024-06-04 21:26:14 -0500

    Match POCOs

---
+++ RankMaster/POCOs/MatchCollection.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RankMaster.POCOs;
+
+[JsonSerializable(typeof(MatchCollection))]
+public class MatchCollection
+{
+    [JsonPropertyName("data")]
+    public IEnumerable<Match> Matches { get; set; }
+}
